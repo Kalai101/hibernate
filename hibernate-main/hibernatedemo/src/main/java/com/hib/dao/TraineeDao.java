26277d3faa84baf8e5ee7b11a4ebb1ab1db52fc4
KALAI <90127679+Kalai101@users.noreply.github.com>
2022-09-30 10:08:32 +0530

    Delete hibernate-main/hibernatedemo directory

--- hibernate-main/hibernatedemo/src/main/java/com/hib/dao/TraineeDao.java
+++
@@ -1,7 +0,0 @@
-package com.hib.dao;
-
-import com.hibernate.model.Trainee;
-
-public interface TraineeDao {
-	public void createTrainee(Trainee t);
-}
--- hibernate-main/hibernatedemo/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Welcome Trainees!</h2>
-<a href = "register.jsp">Register here</a>
-</body>
-</html>
